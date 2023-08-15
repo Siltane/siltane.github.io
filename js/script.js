c67a032732eefc1b4dd1df4f6fecbf6c087da8cd
Siltane <69503142+Siltane@users.noreply.github.com>
2023-08-15 15:43:35 -0400

    asdasda

--- js/script.js
+++ js/script.js
@@ -2,7 +2,19 @@
 document.addEventListener("DOMContentLoaded", function() {
     var themeButton = document.getElementById("theme-toggle");
 
+    // Load saved theme preference
+    if (localStorage.getItem("theme") === "dark-mode") {
+        document.body.classList.add("dark-mode");
+    }
+
     themeButton.addEventListener("click", function() {
         document.body.classList.toggle("dark-mode");
+        
+        // Save theme preference
+        if (document.body.classList.contains("dark-mode")) {
+            localStorage.setItem("theme", "dark-mode");
+        } else {
+            localStorage.removeItem("theme");
+        }
     });
 });
